57ff9a8ea089668f2fa41762f383adfc928a8f34
Davtorresga06 <davtorresga@unal.edu.co>
2025-04-28 18:23:36 -0500

    Ejercicio de los descuentos

--- Clase_28_Abril.py
+++ Clase_28_Abril.py
@@ -29,14 +29,34 @@ print("El mayor de los 2 muneros es: ", Mayor_de_2_numeros(a,b))
 
 
 ## OPERADOR CONDICIONAL TERNARIO
-def Valor_abosoluto(r):
-    return r * -1 if r < 0 else r
-r = int(input("Ingrese un numero real para conocer su valor absoluto: "))
-print("el valor absoluto es: ",Valor_abosoluto(r))
 
-def Mayor_de_2_numeros(a,b):
-    return a if a > b else b
-a = float(input("Ingrese un numero real: "))
-b = float(input("Ingrese un numero real: "))
-print("El mayor de los 2 muneros es: ", Mayor_de_2_numeros(a,b))
 
+
+## EJECICIO DE LOS PRODUCTOS CON DESCUENTO
+def precio_final(precio_inicial,n_productos,descuento):
+    if n_productos < 5:
+        return precio_inicial
+    elif 5 < n_productos <= 10:
+        return precio_inicial * descuento
+    elif 10 < n_productos <= 15:
+        return precio_inicial * descuento
+    elif 15 < n_productos <= 20:
+        return precio_inicial * descuento
+    else:
+        return precio_inicial * descuento
+    
+a = 0.95
+b= 0.90
+c = 0.85
+d = 0.80
+
+precio_inicial = float(input("Ingrese el precio del producto (COP): "))
+n_productos = int(input("Ingrese la cantidad de productos: "))
+descuento = (
+    0 if n_productos < 5 else
+    a if 5 <= n_productos <= 10 else
+    b if 10 < n_productos <= 15 else
+    c if 15 < n_productos <= 20 else
+    d
+) 
+print("El precio final es: ", precio_final(precio_inicial,n_productos,descuento) * n_productos ,"Y el descuento es del :", 1 - descuento , "%")
