4343782e506123944e2ae1162e2073913ce67b9a
Davtorresga06 <davtorresga@unal.edu.co>
2025-04-28 13:41:18 -0500

    clase 28 abril

---
+++ Clase_28_Abril.py
@@ -0,0 +1,17 @@
+#hola mundo
+##Se pueden hacer commits de visual a git y de git a visual, pero dice que se debe hace un pull
+##para trabajar con la ultima version del repositorio que es la que esta en git, haciendo que se combinen los commits de visual y de git, esta operacion se llama merge)
+
+
+
+##ESTRUCTURAS DE COCNTROL(if, else, elif)
+## los condiconales son solo verdadero y falso(booleanos)
+
+z = 3 * 3
+
+if(z == 9):
+    print("Se ejecuta el condicional")
+else:
+    print("No se ejecuta el condicional")
+    
+
