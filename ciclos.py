23bc8bc7d79682211fc63c942a7379e30316cb03
Davtorresga06 <davtorresga@unal.edu.co>
2025-04-30 14:53:24 -0500

    clase 30 de abril

---
+++ ciclos.py
@@ -0,0 +1,27 @@
+## Estructura para realizar ciclos while en python
+
+x = 0
+y = 10
+
+while (x < 5 and y > 5):
+    print("hola ", x)
+    print("adios ", y)
+    y -= 1
+    
+print("fin del ciclo")
+#siempre dar condiciones con fin al ciclo, si no se queda en un bucle infinito
+## Esta es la estructura para realizar ciclos en python 
+
+## Estructura para realizar ciclos for en python
+##            0        1         2        3
+Frutas = ["manzana", "pera", "naranja", "uva"]
+
+print("Frutas: ", Frutas[1])
+
+for i in Frutas:
+    print("Esta frutas es: ", i)
+
+##operaciones de conversion
+
+print("este es el quivalente a 65 ",chr(65)) #convierte el valor 65 a su respectivo caracter ascci
+
