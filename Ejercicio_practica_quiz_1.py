21237ecf24ea8acda698f53596d7248e3cb0b43d
Davtorresga06 <davtorresga@unal.edu.co>
2025-05-06 15:48:59 -0500

    areglar los ejercicios en las notas para el quiz

--- Ejercicio_practica_quiz_1.py
+++ Ejercicio_practica_quiz_1.py
@@ -4,15 +4,16 @@ if numero > 0:
     print("El cuadrado de el número ingresado es", numero ** 2)
 else:  
     print("El numero ingresado es negativo o 0")
-
 n = int(input("Ingrese un número entero positivo: "))
-while n != 1:
+while n != 1:## != significa diferente de
     print(n, end=" ")
-    if n % 2 == 0:
-        n = n // 2
+    if n % 2 == 0:## % se usa para saber el residuo de la dividion de n entre 2(en este caso para que sea par el dividirse en 2 debe ser 0 el ser residuo)
+        n = n / 2
     else:
         n = 3 * n + 1
 print(n)  # Para imprimir el 1 al final
+##end=" " sirve para imprimir el valor de n en la misma línea, separado por espacios, en lugar de que cada número salga en una línea nueva.
+##Sin end=" " se imprimiría en una nueva línea.
 
 año = 2022
 poblacion_A = 25.0
